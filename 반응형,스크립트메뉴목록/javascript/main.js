22c9aabd524ff7114c3a012a7dab35eb4cd6c96d
KimSungJin <86825816+aggie4@users.noreply.github.com>
2022-06-26 23:49:09 +0900

    Delete 반응형,스크립트메뉴목록 directory

--- 반응형,스크립트메뉴목록/javascript/main.js
+++
@@ -1,9 +0,0 @@
-const toggleBth = document.querySelector('.navber__toogleBth');
-const menu = document.querySelector('.navber__menu');
-const icons = document.querySelector('.navber__icons');
-
-toggleBth.addEventListener('click',() => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
-
